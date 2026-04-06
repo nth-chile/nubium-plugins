4d8e8a9a4d12de3f7b7b29cc2d218b5b0afbfa72
Jared Salzano <jaredsalzano@gmail.com>
2026-04-05 21:00:36 -0400

    Update hello-world to v1.1.0, remove note-counter

--- plugins/hello-world/dist/index.js
+++ plugins/hello-world/dist/index.js
@@ -1,14 +1,18 @@
 var HelloWorldPlugin = {
   id: "hello-world",
   name: "Hello World",
-  version: "1.0.0",
-  description: "A simple test plugin that adds a greeting command",
-
+  version: "1.1.0",
+  description: "Greeting commands for Nubium",
   activate: function(api) {
     api.registerCommand("hello-world.greet", "Hello World: Greet", function() {
       api.showNotification("Hello from the community plugin registry!");
     });
-  },
+    api.registerCommand("hello-world.score-info", "Hello World: Score Info", function() {
+      var score = api.getScore();
+      var parts = score.parts.length;
+      var measures = score.parts[0] ? score.parts[0].measures.length : 0;
+      api.showNotification((score.title || "Untitled") + ": " + parts + " part" + (parts !== 1 ? "s" : "") + ", " + measures + " measure" + (measures !== 1 ? "s" : ""));
+    });
+  }
 };
-
 module.exports = HelloWorldPlugin;
--- plugins/hello-world/src/index.ts
+++ plugins/hello-world/src/index.ts
@@ -1,12 +1,19 @@
 const HelloWorldPlugin = {
   id: "hello-world",
   name: "Hello World",
-  version: "1.0.0",
-  description: "A simple test plugin that adds a greeting command",
+  version: "1.1.0",
+  description: "Greeting commands for Nubium",
 
   activate(api: any) {
     api.registerCommand("hello-world.greet", "Hello World: Greet", () => {
       api.showNotification("Hello from the community plugin registry!");
+    });
+
+    api.registerCommand("hello-world.score-info", "Hello World: Score Info", () => {
+      const score = api.getScore();
+      const parts = score.parts.length;
+      const measures = score.parts[0]?.measures.length ?? 0;
+      api.showNotification(`${score.title || "Untitled"}: ${parts} part${parts !== 1 ? "s" : ""}, ${measures} measure${measures !== 1 ? "s" : ""}`);
     });
   },
 };
